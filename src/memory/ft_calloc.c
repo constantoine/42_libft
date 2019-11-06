50a44e3a08b96cc6a7465d6c8584e0db369c49d6
Cleo REBERT <crebert@student.42.fr>
2019-11-06 17:43:55 +0100

    yolo

--- src/memory/ft_calloc.c
+++ src/memory/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: crebert <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 15:40:45 by crebert           #+#    #+#             */
-/*   Updated: 2019/11/04 15:59:52 by crebert          ###   ########.fr       */
+/*   Updated: 2019/11/06 17:37:29 by crebert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,12 @@
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
+	char	*str;
 
 	if (not(ptr = malloc(count * size)))
 		return (NULL);
+	str = (char *)ptr;
+	str[0] = 0;
 	ft_bzero(ptr, count * size);
 	return (ptr);
 }
