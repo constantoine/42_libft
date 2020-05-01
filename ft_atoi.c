fe48b823eea0d0ff65999cdccad6e071bb1a4c36
Cleo Rebert <cleo.rebert@gmail.com>
2020-05-02 00:29:29 +0200

    repaire bug

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: cleo <cleo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 15:05:10 by crebert           #+#    #+#             */
-/*   Updated: 2020/05/01 22:14:38 by cleo             ###   ########.fr       */
+/*   Updated: 2020/05/02 00:28:57 by cleo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ int	ft_atoi(const char *str)
 	int		result;
 
 	while ((sign = 1) && ((*str >= 9 && *str <= 13) || *str == 32))
-		str++ && (result = 0);
-	if (*str == '+' || (*str == '-' && (sign = -1)))
+		str++;
+	if ((result &= ~0xFFFF) && *str == '+' || (*str == '-' && (sign = -1)))
 		str++;
 	while (*str >= '0' && *str <= '9')
 		result = result * 10 + (*str++ - '0') * sign;
