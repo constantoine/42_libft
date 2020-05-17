c4674615f52ff7c8284138638deb228a923f1ded
Cleo Rebert <cleo.rebert@gmail.com>
2020-05-17 13:53:35 +0200

    Itoa now arch independant. Untested

--- ft_itoa.c
+++ ft_itoa.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: crebert <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: cleo <cleo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/05 18:35:44 by crebert           #+#    #+#             */
-/*   Updated: 2019/11/05 19:25:41 by crebert          ###   ########.fr       */
+/*   Updated: 2020/05/17 13:53:06 by cleo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,24 +18,18 @@ char	*ft_itoa(int nb)
 {
 	int		index;
 	int		nbcpy;
-	char	num[12];
+	char	num[50];
 
 	nbcpy = nb;
-	index = 10;
-	num[11] = 0;
-	num[10] = '0';
-	if (nb == -2147483648)
-		num[index--] = '8';
-	if (nb == -2147483648)
+	index = 48;
+	num[49] = 0;
+	num[index] = '0';
+	if (nb == INT_MIN && (num[index--] = '0' + 10 - (INT_MIN % 10)))
 		nb /= 10;
 	if (nb < 0)
 		nb *= -1;
-	while (nb > 0)
-	{
-		num[index] = nb % 10 + '0';
+	while (nb > 0 && (num[index--] = nb % 10 + '0'))
 		nb /= 10;
-		index--;
-	}
 	if (nbcpy < 0)
 		num[index] = '-';
 	else if (nbcpy != 0)
